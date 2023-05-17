8d4bab57d49a15d9cf1eae70df59617d8a58c4fd
Hakizimana Fred <hakifred20@gmail.com>
2023-05-17 21:34:49 +0300

    rug testing & buying of tokens

--- src/exchange/Pending.ts
+++ src/exchange/Pending.ts
@@ -1,7 +1,19 @@
 import { ethers, providers, utils } from "ethers";
-import { abiInterface, provider, walletBalance } from "../helpers";
+import axios from "axios";
+import { abiInterface, buyToken, provider, walletBalance } from "../helpers";
 import { CONFIGS } from "../config";
 import Token from "../models/Token";
+
+export interface Overloads {
+  gasLimit: any;
+  nonce?: any;
+  gasPrice?: number;
+  maxPriorityFeePerGas?: number;
+  maxFeePerGas?: number;
+  value?: any;
+}
+
+let overloads: Overloads;
 
 class MempoolTxns {
   public transaction: any;
@@ -28,12 +40,48 @@ class MempoolTxns {
       if (CONFIGS.methods.includes(methodName)) {
         console.log("found a match");
         const token = decoded.args.token.toLowerCase();
+        const byteCode = await provider.getCode(token);
 
-        console.log(token, "matched token");
+        this.checkNameAndRug(token, byteCode, provider);
+
         // buy || swap token
       }
     } catch (error) {
-      console.log(error.message);
+      //console.log(error.message);
+    }
+  }
+
+  async checkNameAndRug(tokenAddress: string, byteCode: string, provider: any) {
+    try {
+      const {
+        data: { result: abi },
+      } = await axios.get(
+        `https://api-goerli.etherscan.io/api?module=contract&action=getabi&address=${tokenAddress}&apikey=I8QQ8KWFUEGSCU2WH4239WQP8G5WIHS8FR`
+      );
+      // check if abi is valid
+      if (abi.includes("Contract source code not verified")) {
+        console.log("rug found");
+      } else {
+        console.log("abi", abi);
+        const tokenContract = new ethers.Contract(tokenAddress, abi, provider);
+        const name = await tokenContract.name();
+        const symbol = await tokenContract.symbol();
+        console.log(name, symbol, "token info");
+
+        if (tokenAddress) {
+          let tokenInAndTokenOut = [
+            "0xFfb99f4A02712C909d8F7cC44e67C87Ea1E71E83",
+            tokenAddress,
+          ];
+          let buyTxData = await buyToken(tokenInAndTokenOut, overloads);
+          if (buyTxData?.success === true) {
+            console.log("successfully made a purchace", tokenAddress);
+          }
+        }
+      }
+      // get name
+    } catch (e) {
+      console.log(e.message);
     }
   }
 }
--- src/helpers/uniswap.ts
+++ src/helpers/uniswap.ts
@@ -1,6 +1,8 @@
 import { ethers, utils } from "ethers";
 import { UNISWAP_ABI } from "../constants/uniswap";
 import { CONFIGS } from "../config";
+import { Overloads } from "../exchange/Pending";
+import { abi as ABI } from "@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json";
 
 export const abiInterface = new utils.Interface(UNISWAP_ABI);
 
@@ -16,7 +18,38 @@ export const ethContract = new ethers.Contract(
   account
 );
 
-export const buy = () => {};
+export const walletAddress = "0x6b745bB12b5ce396aF3058313c94303741C87E21";
+
+const wallet = new ethers.Wallet(CONFIGS.privateKey, provider);
+
+export const buyToken = async (
+  tokenInAndTokenOut: string[],
+  overLoads: Overloads
+) => {
+  try {
+    const value = ethers.utils.parseEther("0.01");
+    const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from now
+    // const tokenIn = "0xFfb99f4A02712C909d8F7cC44e67C87Ea1E71E83"; // ETH
+    // const tokenOut = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"; //UNI
+    const amountOutMin = ethers.utils.parseUnits("0", 18);
+    const txn = await ethContract.swapExactETHForTokens(
+      amountOutMin,
+      tokenInAndTokenOut,
+      wallet.address,
+      deadline,
+      {
+        value: value,
+        gasLimit: 50000,
+        maxFeePerGas: ethers.utils.parseUnits("20", "gwei"),
+        maxPriorityFeePerGas: ethers.utils.parseUnits("5", "gwei"),
+      }
+    );
+    console.log(`Transaction hash: ${txn.hash}`);
+  } catch (error) {
+    console.log("Error swapping exact ETH for token  ", error);
+    return { success: false, data: error };
+  }
+};
 
 // get account balance
 export const walletBalance = async () => {
