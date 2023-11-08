f39e8b6f38b100f3b4525e5a0fc3ff1011f07c95
Hakizimana Fred <hakifred20@gmail.com>
2023-11-08 13:13:54 +0300

    environments set

--- src/utils/processToken.ts
+++
@@ -1,9 +0,0 @@
-function processToken(tokenAddress: string, name: string, symbol: string) {}
-
-function curry(func: Function) {
-  return function curried(...args: any) {
-    if (args.length >= func.length) {
-    } else {
-    }
-  };
-}
