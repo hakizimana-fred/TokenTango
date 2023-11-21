1baf459ad498d0753ce43f125f21eb408dd6a7fc
Hakizimana Fred <hakifred20@gmail.com>
2023-11-21 12:00:38 +0300

    trynna fix WS issue on server

--- src/lib/exchange/uniswap.ts
+++ src/lib/exchange/uniswap.ts
@@ -7,20 +7,26 @@ const transactionHelper = new TransactionHelper();
 export class MempoolTxns {
   // Retrieve pending transactions from the mempool
   async getPendingTxns() {
-    transactionHelper
-      .getProvider()
-      .on("pending", async (transaction): Promise<void> => {
-        try {
-          const transactionInfo = await transactionHelper.getTransaction(
-            transaction
-          );
-          console.log("simply log transaction info", transactionInfo);
-          // transactionInfo && (await this.processAndDecode(transactionInfo));
-        } catch (e) {
-          logger.log(e.message);
-          throw new Error(e.message);
-        }
-      });
+    transactionHelper.getProvider()
+      .then((provider: any) => {
+        provider.on('pending', async(transaction: any): Promise<void> => {
+          console.log(transaction, 'all transaction')
+        })
+      })
+    // transactionHelper
+    //   .getProvider()
+    //   .on("pending", async (transaction): Promise<void> => {
+    //     try {
+    //       const transactionInfo = await transactionHelper.getTransaction(
+    //         transaction
+    //       );
+    //       console.log("simply log transaction info", transactionInfo);
+    //       // transactionInfo && (await this.processAndDecode(transactionInfo));
+    //     } catch (e) {
+    //       logger.log(e.message);
+    //       throw new Error(e.message);
+    //     }
+    //   });
   }
 
   async processAndDecode(transactionInfo: providers.TransactionResponse) {
